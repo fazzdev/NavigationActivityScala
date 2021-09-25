dbde0c53af1d5d82d9b8e172ab8d142d27bbcad3
Faried Fauzi <fazzdev@gmail.com>
2021-09-25 20:37:10 +0200

    Change compile sdk version to 31

--- app/src/main/scala/fazzdev/android/RichActivity.scala
+++ app/src/main/scala/fazzdev/android/RichActivity.scala
@@ -1,14 +1,9 @@
 package fazzdev.android
 
 import android.app.Activity
-import android.os.AsyncTask
 import android.view.View
 
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
-
 trait RichActivity extends Activity {
-  implicit lazy val executionContext: ExecutionContextExecutor = ExecutionContext.fromExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-
   implicit def toRunnable[T](action: => T): Runnable = new Runnable() {
     def run(): Unit = action
   }
